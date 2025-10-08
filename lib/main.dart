d0de93ca643dbf2e9397024670113ab3e9d881ee
allainomi <z3448761@gmail.com>
2025-10-08 19:04:46 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,61 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
-import 'screens/home.dart';
-import 'screens/settings.dart';
 
-void main() => runApp(IslamicWisdomApp());
-
-class IslamicWisdomApp extends StatefulWidget {
-  @override
-  _IslamicWisdomAppState createState() => _IslamicWisdomAppState();
+void main() {
+  runApp(MyApp());
 }
 
-class _IslamicWisdomAppState extends State<IslamicWisdomApp> {
-  ThemeMode _themeMode = ThemeMode.light;
-  double _fontSize = 16;
-  Locale _locale = Locale('ur');
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Islamic Wisdom',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light().copyWith(
-        primaryColor: Color(0xFF0B4F88),
-        scaffoldBackgroundColor: Colors.white,
-        appBarTheme: AppBarTheme(backgroundColor: Colors.white, elevation: 0, iconTheme: IconThemeData(color: Colors.black87)),
+      title: 'Flutter Counter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      darkTheme: ThemeData.dark(),
-      themeMode: _themeMode,
-      home: HomeScreen(onOpenSettings: () async {
-        final result = await Navigator.push(context, MaterialPageRoute(builder: (_) => SettingsScreen(themeMode: _themeMode, fontSize: _fontSize, locale: _locale)));
-        if (result != null && result is Map) {
-          setState(() {
-            _themeMode = result['themeMode'] ?? _themeMode;
-            _fontSize = result['fontSize'] ?? _fontSize;
-            _locale = result['locale'] ?? _locale;
-          });
-        }
-      }),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
